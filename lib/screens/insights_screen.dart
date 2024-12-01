ef5cd5eedb5514c4c5acec43f9e4ec2b63af2103
mohamedihab <mohamed_ihab.92@hotmail.com>
2024-12-01 16:11:26 +0200

    project structure

---
+++ lib/screens/insights_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class InsightsScreen extends StatelessWidget {
+  const InsightsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
